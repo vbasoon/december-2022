593e90f10eb6134dac89e60237b118572c8d6408
vbasoon <vbasoon@gmail.com>
2022-12-04 16:44:42 +0200

    first commit

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY''some_random_default_string')
-
+# SECRET_KEY = "django-insecure-$3axu1zy%felhmn@=21sc&o+m*23!$77$kw6y$w%rw5^1o&&e&"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
